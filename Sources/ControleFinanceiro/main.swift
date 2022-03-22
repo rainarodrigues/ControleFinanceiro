3c846b98f47c2e1575a34ab04303efefaf062c25
Raina Rodrigues de Lima <rainarodriguesdelima@RainaRodrigues.local>
2022-03-22 11:40:48 -0300

    Switch case

--- Sources/ControleFinanceiro/main.swift
+++ Sources/ControleFinanceiro/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+
+func menssageMenu(){
+    print("Menu Principal:\n1 - Saldo inicial\n2 - Receitas\n3 - Despesas\n4 - Saldos\n5 - Metas\n")
+    print("Digite uma opção:")
+}
+
+menssageMenu()
+
+let optionMenu : Int = Int(readLine()!)!
+
+switch optionMenu {
+    case 0:
+        print("Saldo Inicial")
+    case 1:
+        print("Receitas")
+    case 2:
+        print("Despesas")
+    case 3:
+        print("Saldos")
+    case 4:
+        print("Meta")
+    default:
+        print("Sair")
+}
+
